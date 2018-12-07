aed3442a1e67a42147ff76e7d128e2740ea2d3ab
Bima Ketut Ramuna Putra <37969013+bimtut@users.noreply.github.com>
2018-12-07 13:28:44 +0700

    fix

--- tugas3.js
+++ tugas3.js
@@ -1,8 +1,5 @@
-var nama='jono';
-var peran='Taib';
-var bol = false;
-console.log(nama == false)
-
+var nama='jj';
+var peran='';
 
 if (nama == false) {
     console.log('Nama harus diisi!')
@@ -23,23 +20,3 @@ if (nama == false) {
     console.log("Selamat datang di Dunia Proxytia,", nama)
     console.log("Kamu hanya bisa memilih Ksatria, Tabib atau Penyihir untuk karaktermu")
 }
-
-//logika udah kelar. tinggal perbaik aja mirip di js bin dan pake === atau == yg bener
-/*
-var nama='prapto';
-var peran='Ksatria';
-
-if (nama === false && peran === false) {
-    console.log('Nama harus diisi!')
-}else if(nama != false && peran == false) {
-    console.log("Halo "+nama+", Pilih peranmu untuk memulai game!")
-}else if(nama !== false && peran == 'Ksatria') {
-    console.log("Selamat datang di Dunia Proxytia, "+nama+". \n Halo Ksatria "+nama+" kamu dapat menyerang dengan senjatamu!")
-}else if(nama !== false && peran == 'Tabib') {
-    console.log("Selamat datang di Dunia Proxytia,", nama,"Halo Tabib",nama,"kamu dapat menyerang dengan senjatamu!")
-}else if(nama !== false && peran == 'Penyihir') {
-    console.log("Selamat datang di Dunia Proxytia,", nama,"Halo penyihir",nama,"kamu dapat menyerang dengan senjatamu!")
-}else{
-    console.log('masih salah')
-}
-*/
